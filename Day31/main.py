aabc7a25fd063c69a06cc1ff40e018c13485a7fc
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-08-07 21:09:15 +0430

    read the csv file by pandas.read_csv() and changing it to dictionary and use the orient mode for clearing the numbers in dictionary then save it in data and make random cards by it  and create a function for showing new card when user presses the right and wrong buttons.

--- Day31/main.py
+++ Day31/main.py
@@ -1,5 +1,18 @@
 from tkinter import *
+import pandas
+import random
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("./Day31/french_words.csv")
+words = data.to_dict(orient="records")
+
+
+def random_cards():
+    current_card = random.choice(words)
+    # print(current_card["French"])
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
 
 window = Tk()
 window.title("Flashy")
@@ -8,23 +21,22 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 front_image = PhotoImage(file="./Day31/card_front.png")
 canvas.create_image(400, 263, image=front_image)
-canvas.create_text(400, 150, text="title", font=("Arial", 40, "normal"))
-canvas.create_text(400, 263, text="apple", font=("Arial", 60, "normal"))
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "normal"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "normal"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
 right_image = PhotoImage(file="./Day31/right.png")
-right_button = Button(image=right_image)
+right_button = Button(image=right_image, command=random_cards)
 right_button.grid(column=1, row=1)
 
 wrong_image = PhotoImage(file="./Day31/wrong.png")
-wrong_button = Button(image=wrong_image)
+wrong_button = Button(image=wrong_image, command=random_cards)
 wrong_button.grid(column=0, row=1)
-
 
 # back_image = PhotoImage(file="./Day31/card_back.png")
 # canvas.create_image(400, 263, image=back_image)
 
-
+random_cards()
 
 window.mainloop()
